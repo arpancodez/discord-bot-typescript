72a2f948ed96daf8b3e1b59724404803bfc53617
Arpan <164992371+arpancodez@users.noreply.github.com>
2025-10-04 00:43:38 +0530

    Update guildMemberAdd.ts

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -4,8 +4,8 @@ import { Events } from "discord.js";
 export default {
   name: Events.GuildMemberAdd,
   async execute(member: any) {
-    const channelId = "WELCOME_CHANNEL_ID"; // replace with your welcome channel of discord
-    const roleId = "AUTOROLE_ID"; // replace with default role of discord server
+    const channelId = "CHANNEL_ID"; // replace with your welcome channel of discord
+    const roleId = "AUTOROLEID"; // replace with default role of discord server
 
     const channel = member.guild.channels.cache.get(channelId);
     if (channel) channel.send(`👋 Welcome ${member}!`); // you can customize your welcome msg
